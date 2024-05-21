ef0515a03d35a7b754d74802ef4aba5b7bcaa598
MatheusDEVQA <102087318+MatheusDEVQA@users.noreply.github.com>
2024-05-21 18:06:32 -0300

    Add file environment and update testRunner.js

--- testRunner.js
+++ testRunner.js
@@ -1,25 +1,37 @@
 const newman = require('newman')
 
 let collectionFile
+let environmentFile
 
 const context = process.argv[2]
 const environment = process.argv[3]
 
-try{
-    collectionFile= require(`./context/${context}/${context}.postaman_collection.json`)
+try {
+    collectionFile = require(`./context/${context}/${context}.postman_collection.json`)
+    environmentFile = require(`./context/${context}/${environment}-${context}.postman_environment.json`)
 
     console.log(`Running ${context} tests`)
     runTests(context, environment)
-}catch (err) {
+} catch (err) {
     console.error(`Error loading file to suite '${context}' and environment '${environment}: '${err.message}'`)
     process.exit(1)
 }
 
-function runTests(context, environment){
+function runTests(context, environment) {
+    let reporters = ['junit', 'cli', 'json'];
     newman.run({
         collection: collectionFile,
-        reporters,
+        environment: environmentFile,
         insecure: true,
-        iterationCount: 1        
+        reporters,
+        iterationCount: 1,
+        reporter: {
+            junit: {
+                export: `newman/${context}.xml`
+            },
+            json: {
+                export: `newman/${context}.json`
+            }
+        }
     })
 }
